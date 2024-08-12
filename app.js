46a6c99f6f3470add348256599fceb80d1eeed7b
Jasmin In-naka <81845867+jasmininnaka24@users.noreply.github.com>
2024-08-12 13:35:22 +0800

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ const path = require('path');
 const app = express();
 
 // Serve static files from the directory containing server.js
+app.use(express.static(__dirname));
 
+// Serve index.html for the root URL
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 app.listen(3000, function () {
     console.log('App listening on port 3000');
